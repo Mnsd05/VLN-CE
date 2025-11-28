d6e923634cae3ab4c36f42a819025d040d30aa6d
Mnsd05 <thangngdc03bn@gmail.com>
2025-11-29 03:41:31 +0800

    Further reduce

--- vlnce_baselines/__init__.py
+++ vlnce_baselines/__init__.py
@@ -3,4 +3,4 @@ from vlnce_baselines import (
     recollect_trainer,
 )
 from vlnce_baselines.common import environments
-from vlnce_baselines.models import cma_policy, seq2seq_policy, waypoint_policy
+from vlnce_baselines.models import cma_policy, seq2seq_policy
